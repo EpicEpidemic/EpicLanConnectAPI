1d4a40c3e34f86a930a31cb9c0d48e5ac16ed11c
Mark N Broadhead <marknbroadhead@gmail.com>
2017-09-01 20:49:35 -0600

    Add Spring Boot Project files

---
+++ src/main/java/com/epicepidemic/lanconnectapi/LanConnectApiApplication.java
@@ -0,0 +1,12 @@
+package com.epicepidemic.lanconnectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanConnectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanConnectApiApplication.class, args);
+	}
+}
